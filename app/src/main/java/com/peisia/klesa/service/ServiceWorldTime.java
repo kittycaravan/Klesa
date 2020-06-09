285d37011b877d93d87ac3d7eba849a5ad2b22e9
ahnsungmo <smahn@btlf.co.kr>
2020-06-09 15:36:47 +0900

    코드정리

--- app/src/main/java/com/peisia/klesa/service/ServiceWorldTime.java
+++ app/src/main/java/com/peisia/klesa/service/ServiceWorldTime.java
@@ -1,7 +1,6 @@
 package com.peisia.klesa.service;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -12,11 +11,9 @@ import java.util.TimerTask;
 public class ServiceWorldTime extends Service {
     private MyApp mMyApp;
     private Timer timer;
-    public ServiceWorldTime() {
-    }
+    public ServiceWorldTime() { }
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
     @Override
@@ -37,8 +34,7 @@ public class ServiceWorldTime extends Service {
     public void onDestroy() {
         super.onDestroy();
     }
-
-    private void procCmd(Intent intent){
+    private void procCmd(Intent intent) {
         String cmd = intent.getStringExtra(MyApp.INTENT_KEY_CMD);
         switch (cmd){
             case MyApp.INTENT_VALUE_START_WORLD_TIME:
@@ -48,16 +44,6 @@ public class ServiceWorldTime extends Service {
     }
 
     private void procCmdRunWorldTime(){
-        //todo 세계의 시간을 움직여라 =ㅅ= 흐흐..
-        //아어 뭘로 구현할까.. 타이머?
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                mMyApp.setmWorldTime(mMyApp.getmWorldTime() + MyApp.WORLD_TIME_TERM_MS); // 세계의 시간을 MyApp.WORLD_TIME_TERM_MS 만큼의 텀으로 갱신. 즉 이 단위로 계산을 처리한다는 뜻.
-//                Log.v("ASM","==== ==== 세계의 시간이 흐른다! 신세계의 신 카미너스님! :"+mMyApp.getmWorldTime() + " ms 초");
-//            }
-//        }, MyApp.WORLD_TIME_TERM_MS);
-
         runTimer();
     }
 
